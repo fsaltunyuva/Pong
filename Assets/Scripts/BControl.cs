a9a9d66a9b3e99ba200568149c9fbcc27fbfd289
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2024-09-10 13:36:11 +0300

    Folder and Version Change, Refactoring

--- Assets/Scripts/BControl.cs
+++ Assets/Scripts/BControl.cs
@@ -1,41 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BControl : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         Invoke("GoBall", 1);
     }
 
-    void GoBall(){
+    void GoBall()
+    {
         float rand = Random.Range(0, 2);
-        if (rand < 1){
-        rb2d.AddForce(new Vector2(0.4f, -0.3f));
+        
+        if (rand < 1)
+        {
+            rb2d.AddForce(new Vector2(0.4f, -0.3f));
         }
-       else {
-        rb2d.AddForce(new Vector2(-0.4f, -0.3f));
+        else
+        {
+            rb2d.AddForce(new Vector2(-0.4f, -0.3f));
         }
     }
-    void ResetBall(){
+
+    void ResetBall()
+    {
         rb2d.velocity = Vector2.zero;
         transform.position = Vector2.zero;
     }
-    void RestartGame(){
+
+    void RestartGame()
+    {
         ResetBall();
         Invoke("GoBall", 1);
     }
-        void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.tag=="Inv. Walls Right") {
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Inv. Walls Right"))
+        {
             RestartGame();
             Debug.Log("Left Won");
         }
-        else if(other.gameObject.tag=="Inv. Walls Left") {
+        else if (other.gameObject.CompareTag("Inv. Walls Left"))
+        {
             RestartGame();
-             Debug.Log("Right Won");
+            Debug.Log("Right Won");
         }
     }
 }
