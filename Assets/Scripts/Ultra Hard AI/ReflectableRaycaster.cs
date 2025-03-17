9b596d48dd313e24dfb4a9f52dfea876decbde7d
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-03-17 14:50:13 +0300

    AI Calculates in 1 Frame (in while-loop)

--- Assets/Scripts/Ultra Hard AI/ReflectableRaycaster.cs
+++ Assets/Scripts/Ultra Hard AI/ReflectableRaycaster.cs
@@ -1,6 +1,5 @@
 // Explanation of the script can be found in https://github.com/fsaltunyuva/RayReflection
 
-using System;
 using UnityEngine;
 
 public class ReflectableRaycaster : MonoBehaviour
@@ -14,6 +13,7 @@ public class ReflectableRaycaster : MonoBehaviour
     private int currentVertexIndex = 0;
     [SerializeField] private LayerMask defaultLayerMask;
     [SerializeField] private GameObject debugCircle;
+    [SerializeField] private int maximumAIIteration = 50; // Maximum iteration that AI can calculate
 
     private void Start()
     {
@@ -26,9 +26,51 @@ public class ReflectableRaycaster : MonoBehaviour
     {
         origin = originParam;
         direction = directionParam;
-        isCalculationStarted = true;
+        // isCalculationStarted = true; // This boolean is needed to calculate in Update()
+        
+        // Calculating the hit point in while-loop (in one frame)
+        Debug.LogWarning("Calculation Started.");
+        int overflowCounter = 0; // Preventing while-loop to stuck in infinite iterations (in case of hitting tha ball with the bottom of the player)
+
+        while (overflowCounter < maximumAIIteration)
+        {
+            hit = Physics2D.Raycast(origin, direction, Mathf.Infinity, defaultLayerMask);
+
+            Debug.DrawRay(origin, direction * 10, Color.green);
+            //Debug.Break();
+            
+            if (hit)
+            {
+                if (hit.collider.gameObject.CompareTag("Inv. Walls Up") || hit.collider.gameObject.CompareTag("Inv. Walls Bottom"))
+                {
+                    Debug.DrawRay(hit.point, hit.normal, Color.red); // Draw the normal of the hit point
+            
+                    direction = Vector2.Reflect(direction, hit.normal);
+
+                    Debug.DrawRay(hit.point, direction * 10, Color.blue); 
+                    
+                    origin = hit.point;
+                }
+                // else if (hit.collider.gameObject.CompareTag("AI") || hit.collider.gameObject.CompareTag("Inv. Walls Right"))
+                else if (hit.collider.gameObject.CompareTag("Ultra Hard AI Hit Point Indicator Wall"))
+                {
+                    _ultraHardAIControlScript.MoveToCalculatedPosition(hit.point);
+                    Debug.Log($"The calculated hit point is: {hit.point}");
+                    Instantiate(debugCircle, hit.point, Quaternion.identity);
+                    Debug.LogWarning("Calculation Done.");
+                    break;
+                }
+            }
+
+            overflowCounter++;
+        }
+        
     }
 
+    // To calculate using the Update(), by doing this we allow Unity to go on during the calculation (ball still moves during tha calculation) therefore, AI can be late to respond
+    // https://github.com/fsaltunyuva/Pong/issues/9
+    
+    /*
     void Update()
     {
         if (isCalculationStarted)
@@ -64,4 +106,5 @@ public class ReflectableRaycaster : MonoBehaviour
             }
         }
     }
+    */
 }
